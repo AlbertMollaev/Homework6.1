6e514f57b7324c69b757fcc91dece5fdc3cd1c1c
AlbertM <mollaev702@gmail.com>
2023-07-26 23:03:54 +0300

    Создали репозиторий и решили задачи

---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int GetHighThenZero (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) 
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+
+
+void FillMassive(int [] array) 
+{
+    int length = array.Length;
+    Console.WriteLine("Заполните массив цифрами: ");
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.Write("В массиве [");
+    Console.Write(string.Join(", ", array));
+    Console.Write("]");
+}
+
+
+int GetInput(string text)
+{
+        Console.Write(text);
+        return Convert.ToInt32(Console.ReadLine());
+}
+
+int mNumber = GetInput("Введите размер массива : ");
+int [] array = new int [mNumber];
+FillMassive(array);
+Console.WriteLine($" колличество эллементов больше 0 равно --> {GetHighThenZero(array)}");
---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void GetLine (double B1, double K1, double B2, double K2)
+{
+    double x = (B2 - B1) / (K1 - K2);
+    double y1 = K1 * x + B1;
+    double y2 = K2 * x + B2;
+    Console.WriteLine($"Точки , B1 = {B1}, K1 = {K1}, B2 = {B2}, K2 = {K2} , пересечения двух прямых , заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; : --> ({y1}; {y2} )");
+
+}
+
+double GetInput(string text)
+{
+        Console.Write(text);
+        return Convert.ToDouble(Console.ReadLine());
+}
+
+double B1 = GetInput("Введите значение B1: ");
+double K1 = GetInput("Введите значение K1: ");
+double B2 = GetInput("Введите значение B2: ");
+double K2 = GetInput("Введите значение K2: ");
+
+
+GetLine(B1,K1,B2,K2);
